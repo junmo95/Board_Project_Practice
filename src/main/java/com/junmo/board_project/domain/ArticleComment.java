89324e83c82c9af196ec635451f91948e02eecc2
junmo95 <junmop950@gmail.com>
2023-09-24 23:16:00 +0900

    #7-도메인 구현 및 프로젝트 파일 위치 변경 - 게시글, 댓글 도메인 구현 - 프로젝트 폴더 위치가 올바르지 않았던 것을 고침

---
+++ src/main/java/com/junmo/board_project/domain/ArticleComment.java
@@ -0,0 +1,67 @@
+package com.junmo.board_project.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
+public class ArticleComment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 게시글과 연관관계가 있는데 그냥 Long 타입으로 게시글 ID를 받을 수도 있는데, 여기는 객체지향스럽게 객체로 받는다.
+    @Setter @ManyToOne(optional = false) // 필수적이며, 댓글 삭제되도 게시글에 영향 없어야한다.(cascade 기본이 false)
+    private Article article; // 게시글 ID
+    @Setter @Column(nullable = false, length = 500)
+    private String content; // 본문
+
+    @CreatedDate @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성일시
+    @CreatedBy @Column(nullable = false, length = 100)
+    private String createdBy; // 생성자
+    @LastModifiedDate @Column(nullable = false)
+    private LocalDateTime modifiedAt; // 수정일시
+    @LastModifiedBy @Column(nullable = false, length = 100)
+    private String modifiedBy; // 수정자
+
+    protected ArticleComment() {
+    }
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article, String content) {
+        return new ArticleComment(article, content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ArticleComment that = (ArticleComment) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
